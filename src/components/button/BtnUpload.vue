a3d81966e60f03d64704c32e0a73a02e702dd1d0
Ebenezer Dorneles <ebenezerdorneles@gmail.com>
2022-02-27 13:47:43 -0300

    ✨ Btn para  view route Upload

---
+++ src/components/button/BtnUpload.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="btn-upload">
+        <router-link :to="to" class="link-menu">{{name}}</router-link>
+    </div>
+</template>
+<script>
+export default {
+    name: 'BtnUpload',
+    props:{
+        to: String,
+        name: String,
+    }
+}
+</script>
+
+<style scoped>
+ #click-menu{
+     
+ }
+</style>
