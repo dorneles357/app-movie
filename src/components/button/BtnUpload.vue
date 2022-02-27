677b19d5f720e19da31fcc4a447b0cff348ffcb4
Ebenezer Dorneles <ebenezerdorneles@gmail.com>
2022-02-27 14:46:05 -0300

    💄 Ajustes no BtnUpload e Home

--- src/components/button/BtnUpload.vue
+++ src/components/button/BtnUpload.vue
@@ -1,20 +1,38 @@
 <template>
     <div id="btn-upload">
-        <router-link :to="to" class="link-menu">{{name}}</router-link>
+        <router-link to="/upload" class="link-menu">
+            <p>+</p>
+        </router-link>
     </div>
 </template>
 <script>
 export default {
     name: 'BtnUpload',
-    props:{
-        to: String,
-        name: String,
-    }
 }
 </script>
 
 <style scoped>
- #click-menu{
-     
+ #btn-upload{
+     margin: auto;
+     display: flex;
+     align-items: center;
+     justify-content: center;
+     border: 1px solid var(--color-link);
+     height: 70px;
+     width: 70px;
+     border-radius: 99%;
  }
+ .link-menu{
+     font: var(--font-family);
+     text-align: center;
+     font-size: 80px;
+     font-weight: 900;
+     text-decoration: none;
+     color: var(--color-border);
+ }
+
+ .link-menu:hover{
+     color: var(--color-link);
+ }
+
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,16 @@
 <template>
     <user-header/>
+    <btn-upload/>
 </template>
 
 <script>
-import UserHeader from '../components/headers/UserHeader.vue'
+import UserHeader from '../components/headers/UserHeader.vue';
+import BtnUpload from '../components/button/BtnUpload.vue';
 export default {
     name:'Home',
     components:{
         UserHeader,
-    }
+        BtnUpload,
+    },
 }
 </script>
