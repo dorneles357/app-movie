df7c13b9ce2c7e2887afaee0750564e37965dc23
Ebenezer Dorneles <ebenezerdorneles@gmail.com>
2022-02-27 13:48:02 -0300

    💄 Ajuste no tamanho e padding

--- src/App.vue
+++ src/App.vue
@@ -15,5 +15,13 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  max-width: 1280px;
+  padding: 0px 30px 0px 30px;
+}
+
+@media(max-width:500px) {
+  #app{
+    padding: 0px 10px 0px 10px;
+  }
 }
 </style>
