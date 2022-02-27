8a23977b0d24e050f834a908a37b8b029788cde9
Ebenezer Dorneles <ebenezerdorneles@gmail.com>
2022-02-27 12:22:37 -0300

    🎨 Componetes para Reggister

---
+++ src/components/inputs/RegisterInput.vue
@@ -0,0 +1,52 @@
+<template>
+    <div id="box">
+        <div id="input">
+            <input type="text" name="name" id="name" placeholder="Nome">
+            <input type="email" name="email" id="email" placeholder="Email">
+            <input type="password" name="password" id="psw" placeholder="Senha">
+            <input type="password" name="conf_psw" id="conf_psw" placeholder="Confirme a senha">
+        </div>
+        <button type="submit" id="btn">Entrar</button>
+    </div>    
+</template>
+
+<script>
+export default {
+    name: 'RegisterInput',
+}
+</script>
+
+<style scoped>
+    #box{
+        margin: auto;
+        border: 1px solid var(--color-border);
+        border-radius: 20px;
+        max-height: 500px;
+        max-width: 600px;
+    }
+    #input{
+        display: flex;
+        flex-direction: column;
+    }
+    #input > input{
+        font: var(--font-family);
+        border: 1px solid #716E6E;
+        border-radius: 5px;
+        margin: 20px;
+        max-width: 550px;
+        height: 40px;
+    }
+    #btn{
+        margin-top: 50px;
+        margin-bottom: 20px;
+        font: var(--font-family);
+        height: 40px;
+        width: 100px;
+        border: 1px solid var(--color-border);
+        border-radius: 10px;
+    }
+    #btn:hover{
+        color: var(--color-link);
+    }
+
+</style>
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,13 +1,16 @@
  <template>
-    <LoginAndRegisterLogin />
+    <login-and-register-login />
+    <register-input />
 </template>
 
 <script>
-import LoginAndRegisterLogin from '../components/headers/LoginAndRegisterLogin.vue'
+import LoginAndRegisterLogin from '../components/headers/LoginAndRegisterLogin.vue';
+import RegisterInput from '../components/inputs/RegisterInput.vue';
 export default {
     name:'Register',
     components:{
         LoginAndRegisterLogin,
+        RegisterInput,
     }
 }
 </script>
