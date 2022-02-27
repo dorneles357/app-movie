06d362f9ac18ac6c0dab4f5214bf3e161c63fa75
Ebenezer Dorneles <ebenezerdorneles@gmail.com>
2022-02-27 16:04:05 -0300

    ✅ Profile e edição de usuários

---
+++ src/components/inputs/ProfileInput.vue
@@ -0,0 +1,62 @@
+<template>
+    <div id="box">
+        <div id="input">
+            <input type="text" name="name" id="name" :placeholder="api.name">
+            <input type="email" name="email" id="email" :placeholder="api.email">
+            <input type="password" name="password" id="psw" placeholder="Senha">
+            <input type="password" name="conf_psw" id="conf_psw" placeholder="Confirme a senha">
+        </div>
+        <button type="submit" id="btn">Entrar</button>
+    </div>    
+</template>
+
+<script>
+export default {
+    name: 'RegisterInput',
+    data(){
+        return{
+            api:
+                {
+                    name:"Ebenézer",
+                    email: "ebenezerdorneles@gmail.com",
+                }
+        }
+    }
+}
+</script>
+
+<style scoped>
+    #box{
+        padding-top:30px;
+        margin: auto;
+        border: 1px solid var(--color-border);
+        border-radius: 20px;
+        height: 500px;
+        max-width: 600px;
+    }
+    #input{
+        display: flex;
+        flex-direction: column;
+    }
+    #input > input{
+        font: var(--font-family);
+        border: 1px solid var(--color-border);
+        border-radius: 5px;
+        margin: 20px;
+        max-width: 550px;
+        height: 40px;
+    }
+    #btn{
+        margin-top: 50px;
+        margin-bottom: 20px;
+        font: var(--font-family);
+        height: 40px;
+        width: 100px;
+        border: 1px solid var(--color-border);
+        border-radius: 10px;
+    }
+    #btn:hover{
+        color: var(--color-link);
+    }
+
+</style>
--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,13 +1,17 @@
 <template>
     <user-header/>
+    <profile-input/>
 </template>
 
 <script>
-import UserHeader from '../components/headers/UserHeader.vue'
+import UserHeader from '../components/headers/UserHeader.vue';
+import ProfileInput from '../components/inputs/ProfileInput.vue';
+
 export default {
     name:'Profile',
     components:{
         UserHeader,
+        ProfileInput,
     }
 }
 </script>
