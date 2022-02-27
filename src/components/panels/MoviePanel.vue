205f54e689a94074840ab9312554c4e076ee55e5
Ebenezer Dorneles <ebenezerdorneles@gmail.com>
2022-02-27 16:28:28 -0300

    🎨 Painel para exibição de conteudo

---
+++ src/components/panels/MoviePanel.vue
@@ -0,0 +1,53 @@
+<template>
+<div class="modal">
+    <h3>{{api.name}}</h3>
+    <div class="modal__header">
+    <p>{{api.type}}</p>
+    <p>{{api.size}}</p>
+    </div>
+    ...<!--TAGS-->..
+    <div class="modal__footer">
+    <!--Btn DELETAR e EDITAR -->
+    ...
+    </div>
+</div> 
+</template>
+
+<script>
+export default {
+    name: 'MoviePanel',
+    data(){
+        return{
+            api:
+                {
+                    name: "Meu primeiro video",
+                    type: '.mp4',
+                    size: '3mb'
+                }
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+.modal {
+    max-width: 150px;
+    border: 1px solid var(--color-border);
+    border-radius: 4px;
+}
+
+.modal__header {
+    display: flex;
+    justify-content: space-around;
+    font-size: 10px;
+    border-bottom: 1px solid var(--color-border);
+}
+
+.modal__footer {
+    display: flex;
+    justify-content: flex-end;
+    border-top: 1px solid var(--color-border);
+}
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,19 @@
 <template>
     <user-header/>
     <btn-upload/>
+    <movie-panel/>
 </template>
 
 <script>
 import UserHeader from '../components/headers/UserHeader.vue';
 import BtnUpload from '../components/buttons/BtnUpload.vue';
+import MoviePanel from '../components/panels/MoviePanel.vue';
 export default {
     name:'Home',
     components:{
         UserHeader,
         BtnUpload,
+        MoviePanel,
     },
 }
 </script>
