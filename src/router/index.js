2ef883b95118bcdbfe85d48fce9c22de6b887127
Ebenezer Dorneles <ebenezerdorneles@gmail.com>
2022-02-27 11:05:13 -0300

    ✅ Routes das Views(Login, Register)

--- src/router/index.js
+++ src/router/index.js
@@ -3,18 +3,21 @@ import Home from '../views/Home.vue'
 
 const routes = [
   {
+    path: '/login',
+    name: 'Login',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Login.vue')
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Register.vue')
+  },
+  {
     path: '/',
     name: 'Home',
     component: Home
   },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+
 ]
 
 const router = createRouter({
