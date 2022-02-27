e419143da0bb9cdcd46c5f9e58e0023a0fc5e801
Ebenezer Dorneles <ebenezerdorneles@gmail.com>
2022-02-27 13:48:33 -0300

    ✨ Header para usuários

---
+++ src/components/headers/UserHeader.vue
@@ -0,0 +1,46 @@
+<template>
+    <div id="nav-box">
+        <div id="home">
+            <router-link to="/home" class="link-menu" id="name">MovieApp</router-link>
+        </div>
+        <div id="nav-bar">
+            <router-link to="/profile" class="link-menu" id='profile'>Perfil</router-link>
+            <router-link to="/logout" class="link-menu">Sair</router-link>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'UserHeader',
+}
+</script>
+
+<style scoped>
+#nav-box{
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+}
+#nav-bar{
+    display: flex;
+    justify-content: space-between;
+}
+
+.link-menu{
+    font: var(--font-family);
+    text-decoration: none;
+    color: var(--color-font);
+    padding: 20px 0px 20px 0px;
+}
+.link-menu:hover{
+    color: var(--color-link);
+}
+#name{
+    font-weight: 600;
+}
+#profile{
+    padding: 20px 50px 20px 0px;
+}
+
+</style>
