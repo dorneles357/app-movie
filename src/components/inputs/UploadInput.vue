f62a46a442b921f976606f7ad8b00573445bff76
Ebenezer Dorneles <ebenezerdorneles@gmail.com>
2022-02-27 21:51:08 -0300

    ✅ View Upload de arquivos

---
+++ src/components/inputs/UploadInput.vue
@@ -0,0 +1,92 @@
+<template>
+    <div id="box-upload">
+        <div class="container">
+            <!-- The real file input -->
+            <input type="file" class="container__input" />
+
+            <!-- The upload icon -->
+            <div class="container__icon"></div>
+
+            <!-- The label -->
+        </div>
+        <div id="input">
+            <input type="text" name="name" id="name" placeholder="Nome">
+        </div>
+
+        <button type="submit" id="btn">Enviar</button>
+
+        <div id="msg">
+            <h3>Esperando arquivo ...</h3>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'UploadInput',    
+}
+</script>
+
+<style scoped>
+#box-upload{
+    padding-top:30px;
+    margin: auto;
+    border: 1px solid var(--color-border);
+    border-radius: 20px;
+    height: 500px;
+    max-width: 600px;
+}
+.container {
+    height: 25px;
+    margin: auto;
+    position: relative;
+    max-width: 550px;
+    align-items: center;
+    display: flex;
+
+    border: 1px solid var(--color-border);
+    border-radius: 10px;
+}
+
+.container__input {
+
+    height: 25px;
+    left: 0;
+    position: absolute;
+    top: 0;
+    width: 100%;
+
+}
+
+#input{
+    display: flex;
+    flex-direction: column;
+}
+#input > input{
+    font: var(--font-family);
+    border: 1px solid var(--color-border);
+    border-radius: 5px;
+    margin: 20px;
+    max-width: 550px;
+    height: 40px;
+}
+
+#btn{
+    margin-top: 50px;
+    margin-bottom: 20px;
+    font: var(--font-family);
+    height: 40px;
+    width: 100px;
+    border: 1px solid var(--color-border);
+    border-radius: 10px;
+}
+#btn:hover{
+    color: var(--color-link);
+}
+
+#msg{
+    margin-top: 30px;
+    padding-top: 30px;
+}
+
+</style>
--- src/views/Upload.vue
+++ src/views/Upload.vue
@@ -1,13 +1,16 @@
 <template>
     <user-header/>
+    <upload-input/>
 </template>
 
 <script>
 import UserHeader from '../components/headers/UserHeader.vue';
+import UploadInput from '../components/inputs/UploadInput.vue';
 export default {
-    name:'Upload',
+    name:'UploadMovie',
     components:{
         UserHeader,
-    },
+        UploadInput,
+    }
 }
 </script>
