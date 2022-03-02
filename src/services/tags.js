7edfe743606a477f58ac45d573dbc25beca22d8f
Ebenezer Dorneles <ebenezerdorneles@gmail.com>
2022-03-02 14:57:04 -0300

    📝 Configurações com a API

--- src/services/tags.js
+++ src/services/tags.js
@@ -1,9 +1,6 @@
 import {http} from './config';
 
 export default  {
-    show:(id)=>{
-        return http.get(`tags/${id}`);
-    },
     store:(id, tag)=>{
         return http.post(`tags/create/${id}`, tag);
     },
