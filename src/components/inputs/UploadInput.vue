62d68496b8e35e41d34d524ec84962125e2a83db
Ebenezer Dorneles <ebenezerdorneles@gmail.com>
2022-03-02 21:02:41 -0300

    🎨 ajuste no layout

--- src/components/inputs/UploadInput.vue
+++ src/components/inputs/UploadInput.vue
@@ -1,18 +1,19 @@
 <template>
     <form id="box-upload" enctype="multipart/form-data">
-        <form >
+        <form class="input" >
             <br>
             <input type="file"  multiple ref="files" @change.capture.prevent="submitEvent">
             <br>
             <br>
         </form>
-        <form @submit.stop.prevent="submit">
+        <form @submit.stop.prevent="submit" class="input">
             <br>
-            <input type="text" placeholder="Titulo" name="name" id="name" v-model="name" >
+            <input type="text" placeholder="Titulo" name="name" id="name" v-model="name">
             <br>
             <br>
-            <button type="submit">enviar</button>
+            <button type="submit" id="btn">enviar</button>
         </form>
+        <h1 id="msg">Esperando arquivo ...</h1>
     </form>
 
 </template>
@@ -30,16 +31,21 @@ export default {
     methods:{
             submitEvent(event){
                 this.arquive = event.target.files[0];
-                console.log(this.arquive)
             },
             submit(){
               let form  = new FormData();
                 form.append('file', this.arquive);
                 form.append('name', this.name);
                 
-                console.log(this.arquive)
-             Movie.store(form).then(res =>{
-                    console.log(res);
+    
+                Movie.store(form).then(res =>{
+                    if(res.data && res.data.message === "created"){
+                        document.getElementById('msg').innerHTML = "Ok"
+                    }else{
+                        document.getElementById('msg').innerHTML = "Campo inválido, confira as informações"
+                    }
+
+                    setTimeout(()=>{document.getElementById('msg').innerHTML = "Esperando arquivo ..."}, 3000)
                 }) 
 
             }
@@ -56,6 +62,35 @@ export default {
     height: 500px;
     max-width: 600px;
 }
+.input{
+    display: flex;
+    flex-direction: column;
+}
+.input > input{
+    font: var(--font-family);
+    border: 1px solid var(--color-input-border);
+    border-radius: 5px;
+    margin: 20px;
+    max-width: 550px;
+    height: 30px;
+}
+#btn{
+    margin:auto;
+/*         margin-top: 50px;
+    margin-bottom: 20px; */
+    font: var(--font-family);
+    height: 40px;
+    width: 100px;
+    border: 1px solid var(--color-border);
+    border-radius: 10px;
+}
+#btn:hover{
+    color: var(--color-link);
+}
 
+#msg{
+    padding-top: 50px;
+    margin-top: 40px;
+}
 
 </style>
